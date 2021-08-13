579b8caca85b2a844d81309fcce3d003973dcdda
MGrin <mr6r1n@gmail.com>
2021-08-13 10:49:50 +0300

    Updates

---
+++ precache-manifest.135e743c70ea5ff4abb794dab471a4c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9976a2b63f318043b571e5814351f5b4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a18cba1d3e180a8cc7ac",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "f1be98092af064dc20e7",
+    "url": "./static/js/2.5702a6da.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.5702a6da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a18cba1d3e180a8cc7ac",
+    "url": "./static/js/main.deb938c3.chunk.js"
+  },
+  {
+    "revision": "66508e924bd1d174447c",
+    "url": "./static/js/runtime-main.76d5cc5a.js"
+  }
+]);
