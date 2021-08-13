8caf880907d20b645c535f4d7a347172236a6226
MGrin <mr6r1n@gmail.com>
2021-08-13 12:34:37 +0300

    update example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,78 +1,125 @@
-import React, { useState, useCallback } from 'react'
+import React, { useCallback, useEffect } from 'react'
 
 import useSoundWaves from 'use-sound-waves'
 
-type ButtonsProps = {
-  onClick: (freq: number) => void
+type KeyProps = {
+  name: string
+  frequency: number
+  keyboardKey: string
+  secondary?: boolean
 }
-const Buttons = ({ onClick }: ButtonsProps) => {
+
+const Key = ({ frequency, secondary, keyboardKey }: KeyProps) => {
+  const { isPlaying, play, stop } = useSoundWaves()
+
+  const onMouseDown = useCallback(() => {
+    play({ frequency })
+  }, [play, frequency])
+
+  const onMouseUp = useCallback(() => {
+    stop()
+  }, [stop])
+
+  useEffect(() => {
+    const onKeyDown = (e: Event) => {
+      if ((e as KeyboardEvent).key === keyboardKey) {
+        onMouseDown()
+      }
+    }
+
+    const onKeyUp = (e: Event) => {
+      if ((e as KeyboardEvent).key === keyboardKey) {
+        onMouseUp()
+      }
+    }
+
+    document.addEventListener('keydown', onKeyDown)
+    document.addEventListener('keyup', onKeyUp)
+
+    return () => {
+      document.removeEventListener('keydown', onKeyDown)
+      document.removeEventListener('keyup', onKeyUp)
+    }
+  }, [onMouseDown, onMouseUp, keyboardKey])
+
   return (
-    <>
-      <button onClick={() => onClick(30)}>30</button>
-      <button onClick={() => onClick(35)}>35</button>
-      <button onClick={() => onClick(40)}>40</button>
-      <button onClick={() => onClick(45)}>45</button>
-      <button onClick={() => onClick(50)}>50</button>
-      <button onClick={() => onClick(55)}>55</button>
-      <button onClick={() => onClick(60)}>60</button>
-    </>
+    <div
+      className={`key ${isPlaying ? 'active' : ''} ${
+        secondary ? 'secondary' : ''
+      }`}
+      onMouseDown={onMouseDown}
+      onMouseUp={onMouseUp}
+    >
+      <span className='button'>{keyboardKey}</span>
+    </div>
   )
 }
+
+const notes = [
+  {
+    name: 'A4',
+    frequency: 440,
+    secondary: false,
+    key: 'a'
+  },
+  {
+    name: 'A4A♯4/B♭4',
+    frequency: 466.1638,
+    secondary: true,
+    key: 'w'
+  },
+  {
+    name: 'B4',
+    frequency: 493.8833,
+    secondary: false,
+    key: 's'
+  },
+  {
+    name: 'C5',
+    frequency: 523.2511,
+    secondary: false,
+    key: 'd'
+  },
+  {
+    name: 'C♯5/D♭5',
+    frequency: 554.3653,
+    secondary: true,
+    key: 'e'
+  },
+  {
+    name: 'D5',
+    frequency: 587.3295,
+    secondary: false,
+    key: 'f'
+  },
+  {
+    name: 'D♯5/E♭5',
+    frequency: 622.254,
+    secondary: true,
+    key: 'r'
+  },
+  {
+    name: 'E5',
+    frequency: 659.2551,
+    secondary: false,
+    key: 'g'
+  },
+  {
+    name: 'F5',
+    frequency: 698.4565,
+    secondary: false,
+    key: 'h'
+  }
+]
+
 const App = () => {
-  const [randomFreq, setRandomFreq] = useState(20)
-
-  const player1 = useSoundWaves()
-  const player2 = useSoundWaves()
-  const player3 = useSoundWaves()
-
-  const playFrequency1 = useCallback(
-    (freq) => {
-      player1.play({ frequency: Number(freq), type: 'sine' })
-    },
-    [player1]
-  )
-  const playFrequency2 = useCallback(
-    (freq) => {
-      player2.play({ frequency: Number(freq) })
-    },
-    [player2]
-  )
-  const playFrequency3 = useCallback(() => {
-    player3.play({ frequency: Number(randomFreq) })
-  }, [player3, randomFreq])
-
-  const changeRandomFreq = useCallback((e) => {
-    setRandomFreq(e.target.value)
-  }, [])
-
   return (
     <div className='app'>
-      <h1>useSoundWaves</h1>
-      <Buttons onClick={playFrequency1} />
-      <p>{JSON.stringify(player1.currentSound)}</p>
-      <button onClick={player1.stop} disabled={!player1.isPlaying}>
-        Stop
-      </button>
-      <hr />
-      <Buttons onClick={playFrequency2} />
-      <p>{JSON.stringify(player2.currentSound)}</p>
-      <button onClick={player2.stop} disabled={!player2.isPlaying}>
-        Stop
-      </button>
-      <hr />
-      <input
-        style={{ width: 600 }}
-        type='range'
-        min={20}
-        max={20000}
-        value={randomFreq}
-        onChange={changeRandomFreq}
-      />
-      <span>{randomFreq}</span>
-      <br />
-      <button onClick={player3.isPlaying ? player3.stop : playFrequency3}>
-        {player3.isPlaying ? 'Stop' : 'Play'}
-      </button>
+      <div className='keyboard'>
+        {notes.map((note) => (
+          <Key {...note} keyboardKey={note.key} key={note.key} />
+        ))}
+      </div>
     </div>
   )
 }
